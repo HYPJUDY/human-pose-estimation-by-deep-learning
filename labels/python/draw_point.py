fd3266b9db72fbf9474699ed3be0b8af3848d775
HYPJUDY <378567879@qq.com>
2017-08-21 14:33:06 +0800

    support joints order visualization

--- labels/python/draw_point.py
+++ labels/python/draw_point.py
@@ -10,11 +10,8 @@ import matplotlib.pyplot as plt
 
 
 def get_point(a_list, idx):
-    # w, h = a_list[idx * 2: idx * 2 + 2]
-    w = float(a_list[idx * 2]) * 256
-    h = float(a_list[idx * 2 + 1]) * 212
+    w, h = a_list[idx * 2: idx * 2 + 2]
     return int(float(w)), int(float(h))
-
 
 def draw_imgs(src_dir, dst_dir, annos_file):
     if not os.path.exists(dst_dir):
@@ -27,18 +24,22 @@ def draw_imgs(src_dir, dst_dir, annos_file):
                 print ("Processing ", count, " images.")
 
             tmp = re.split(" |,", line.strip())
-            if(len(tmp) != 32): # 40
+            if(len(tmp) != 32):
                 print(len(tmp))
                 print ("Length of Data Error")
                 sys.exit(0)
             filename = tmp[0]
             coors = tmp[1:39]
             img = cv2.imread(src_dir + filename, 0)
-            for idx in xrange(15): # 19
+            print ('drawing image', filename)
+            for idx in xrange(15):
                 w, h = get_point(coors, idx)
                 cv2.circle(img, (w, h), 2, 255)
-            #plt.imshow(img, cmap='gray')
-            #plt.show()
+                # visualization of the order of 15 joints corresponding to labels (annos file)
+                print ('coordinate of point #', idx + 1, ': (', w, ',', h, ')')
+                cv2.putText(img, str(idx + 1), (w, h), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255))
+            plt.imshow(img, cmap='gray')
+            plt.show()
             cv2.imwrite(dst_dir + filename, img)
 
 def main():
@@ -51,7 +52,7 @@ def main():
 
     src_dir = "../../data/input/" + PHASE + "_imgs" + TAG + "/"
     dst_dir = "../../data/output/" + PHASE + "_imgs" + TAG + "/"
-    annos_file = "../txt/output/" + PHASE + "_annos" + TAG + ".txt"
+    annos_file = "../txt/input/" + PHASE + "_annos" + TAG + ".txt"
     
     print ("start")
     draw_imgs(src_dir, dst_dir, annos_file)
